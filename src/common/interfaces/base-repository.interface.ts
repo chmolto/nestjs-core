b782b6d522332da001721a4e2cddb1587c5d5b64
Christian Molto <christian.molto@aktrion.com>
2025-07-16 18:42:10 +0200

    feat: update operation options

--- src/common/interfaces/base-repository.interface.ts
+++ src/common/interfaces/base-repository.interface.ts
@@ -1,15 +1,30 @@
 import { SearchRequest } from "./search-request.interface";
 
 export interface OperationOptions<Transaction> {
+  // populate all related fields
   populate?: boolean;
+  // only populate selected fields
+  populateFields?: string[];
+  // ex. updateById doesnt find id
   raiseException?: boolean;
+  // to bind multiple queries ex. 1 fails all fail
   transaction?: Transaction;
+  // columns to exclude from the query
+  dontSelectFields?: string[];
+  // to select specific fields
+  selectFields?: string[];
 }
 
 export interface IBaseRepository<T, CreateDTO, UpdateDTO, Transaction> {
-  create(createDto: CreateDTO, options?: OperationOptions<Transaction>): Promise<T>;
+  create(
+    createDto: CreateDTO,
+    options?: OperationOptions<Transaction>
+  ): Promise<T>;
   findAll(options?: OperationOptions<Transaction>): Promise<T[]>;
-  findMany(filters: Record<string, any>, options?: OperationOptions<Transaction>): Promise<T[]>;
+  findMany(
+    filters: Record<string, any>,
+    options?: OperationOptions<Transaction>
+  ): Promise<T[]>;
   findByPagination(searchRequest: SearchRequest): Promise<{
     data: T[];
     page: number;
@@ -30,6 +45,12 @@ export interface IBaseRepository<T, CreateDTO, UpdateDTO, Transaction> {
     updateDto: UpdateDTO,
     options?: OperationOptions<Transaction>
   ): Promise<T>;
-  deleteById(id: string | number, options?: OperationOptions<Transaction>): Promise<{ message: string }>;
-  deleteMany(ids: (string | number)[], options?: OperationOptions<Transaction>): Promise<{ message: string }>;
+  deleteById(
+    id: string | number,
+    options?: OperationOptions<Transaction>
+  ): Promise<{ message: string }>;
+  deleteMany(
+    ids: (string | number)[],
+    options?: OperationOptions<Transaction>
+  ): Promise<{ message: string }>;
 }
